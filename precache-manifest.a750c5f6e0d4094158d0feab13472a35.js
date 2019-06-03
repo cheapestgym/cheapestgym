9b96cec04a98a3fd3860e07b942b9428d13f6811
englishdapp <englishdapp@gmail.com>
2019-06-04 03:19:10 +0900

    Updates

---
+++ precache-manifest.a750c5f6e0d4094158d0feab13472a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2be9e7d53502133dadb4b84791f4a9",
+    "url": "/cheapestgym/index.html"
+  },
+  {
+    "revision": "f4018b18f7910f82cb51",
+    "url": "/cheapestgym/static/css/2.5ccb918e.chunk.css"
+  },
+  {
+    "revision": "0d8c043c915bf4437686",
+    "url": "/cheapestgym/static/css/main.5d17eb6f.chunk.css"
+  },
+  {
+    "revision": "f4018b18f7910f82cb51",
+    "url": "/cheapestgym/static/js/2.ea34ae72.chunk.js"
+  },
+  {
+    "revision": "0d8c043c915bf4437686",
+    "url": "/cheapestgym/static/js/main.34f01549.chunk.js"
+  },
+  {
+    "revision": "56ba8623d8cb6abc9d97",
+    "url": "/cheapestgym/static/js/runtime~main.1625b345.js"
+  },
+  {
+    "revision": "71c806428f9d8c76f8dd491ee177382c",
+    "url": "/cheapestgym/static/media/marker.71c80642.png"
+  }
+]);
